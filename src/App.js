d458326a066046feed955c43292d3661da0320b2
Arjen <alphabravoab@gmail.com>
2018-06-19 16:23:01 +0200

    menu works

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
-import ChatScreen from './components/ChatScreen'
 import './App.css';
-
+import DiscoverScreen from './components/DiscoverScreen'
+import ProfileScreen from './components/ProfileScreen'
+import ChatScreen from './components/ChatScreen'
+import Menu from './components/Menu'
+import { Router, Route } from 'react-router';
 
 class App extends Component {
   render() {
     return (
+    
       <div className="App">
-        <ChatScreen />
 
+        <Route path="/Discover" component={DiscoverScreen} />
+        <Route path="/Profile" component={ProfileScreen} />
+        <Route path="/Chat" component={ChatScreen} />
 
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Router, Route } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'
 import store from './store'
 
+
 ReactDOM.render(
 <Provider store={store}>
   <BrowserRouter>
+
     <App />
   </BrowserRouter>
 </Provider>,
