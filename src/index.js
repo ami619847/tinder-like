6e02dab5f4cb3383ffe08335c00a124d9557d429
Arjen <alphabravoab@gmail.com>
2018-06-19 09:50:40 +0200

    added basics2

--- src/index.js
+++ src/index.js
@@ -4,5 +4,11 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+eactDOM.render(
+<Provider store={store}>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+</Provider>,
+  document.getElementById('root')
+);
