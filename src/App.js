a8c3b06d94f289d37e60571a0996d27f7581f1cb
Arjen <alphabravoab@gmail.com>
2018-06-19 15:31:19 +0200

    its working .gif

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ChatScreen from './components/ChatScreen'
 import './App.css';
-import Test from './components/Test' //just to test if the actions/reducers work
+
 
 class App extends Component {
   render() {
--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import reducer from './reducers'
 import ReduxThunk from 'redux-thunk'
 
+
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 const enhancer = compose(
