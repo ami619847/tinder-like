3808803f30154c4e9c771516c2ee59c1a80829a9
ARYUNAP <AMI619847@GMAIL.com>
2018-06-19 20:39:40 +0200

    no main page yet

---
+++ src/components/HobbyForm.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react';
+
+export default class HobbyForm extends PureComponent {
+
+  render() {
+    return(
+      <div>
+        {/*this.props.typeHobby
+        <button={this.props.experienceHobby}this.props.experienceHobby: experienceHobby, //radiobutton with 3 choices: beginner, intermediate, experienced
+        this.props.userType
+        */}
+      </div>
+    )
+  }
+}
