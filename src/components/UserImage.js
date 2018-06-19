14d713554eefcc153008ea14c8c66affe60efc8e
ARYUNAP <AMI619847@GMAIL.com>
2018-06-19 17:52:02 +0200

    creating userprofile

---
+++ src/components/UserImage.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react';
+
+export default class UserImage extends PureComponent {
+
+  render() {
+    return(
+      <div>
+        <p>IMAGE</p>
+      </div>
+    )
+  }
+}
