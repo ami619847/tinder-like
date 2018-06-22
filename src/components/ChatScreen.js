1de43220aa3a9f2063e62465c3a65da2f08299c4
Arjen <alphabravoab@gmail.com>
2018-06-22 11:37:05 +0200

    chat looks

--- src/components/ChatScreen.js
+++ src/components/ChatScreen.js
@@ -1,19 +1,28 @@
 import * as React from 'react'
 import Menu from './Menu'
 import { connect } from 'react-redux'
-
+import './ChatScreen.css'
+import Chats from './chatcomponents/Chats'
 
 class ChatScreen extends React.PureComponent {
+
   render() {
     return(<div>
       <Menu />
       <div className= 'chatroom'>
-        <div id="container">
-          <aside id="sidebar">Users</aside>
-          <section id="main">
-            <section id="messages-list">Messages list</section>
-            <section id="new-message">New message</section>
-          </section>
+        <div className = 'matches'>
+
+          <div className = 'icimages'> <img className = 'icimages' src='https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?cs=srgb&dl=face-facial-hair-fine-looking-614810.jpg&fm=jpg' /> </div>
+          <div className = 'icimages'> <img className = 'icimages' src='https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?cs=srgb&dl=adult-beach-beard-736716.jpg&fm=jpg' /> </div>
+          <div className = 'icimages'> <img className = 'icimages' src='https://images.pexels.com/photos/573299/pexels-photo-573299.jpeg?cs=srgb&dl=adolescence-attractive-beautiful-573299.jpg&fm=jpg' /> </div>
+        </div>
+        <div className = 'messages'>
+          Messages:
+          <Chats />
+          <Chats />
+          <Chats />
+          <Chats />
+
         </div>
       </div>
     <p> {console.log(this.props.talker)}
@@ -24,9 +33,10 @@ class ChatScreen extends React.PureComponent {
 }
 
 const mapStateToProps = function (state) {
+  console.log(state.users.currentUser.userMatches)
   return {
-    talker: state.users.userData[1].userName,
-    me: state.users.userData[2].userName,
+    discovered: state.users.userData[1].userName,
+
   }
 }
 
